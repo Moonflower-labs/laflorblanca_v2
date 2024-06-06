80755e7f0f205c06b43cfa2ab1b38fa71d208eb6
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2024-06-06 18:03:48 +0100

    fix review view

--- members/views.py
+++ members/views.py
@@ -157,14 +157,13 @@ class ReviewViewset(viewsets.ViewSet):
         if existing_review:
             return Response('You already have a review', status=status.HTTP_403_FORBIDDEN)
 
-        serializer = self.get_serializer(data=request.data)
+        serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
 
         # Set the user before saving the serializer
         serializer.save(user=request.user)
 
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @method_decorator(csrf_protect, name='dispatch')
