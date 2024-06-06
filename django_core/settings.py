7c0868218c078185088d8c14a40b85989e6d7962
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2024-06-06 17:56:59 +0100

    updated settings

--- django_core/settings.py
+++ django_core/settings.py
@@ -197,13 +197,12 @@ STATICFILES_DIRS = [
 
 # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
