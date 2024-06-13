aed0be500fc02193ff5ee32bd990670cfa163ff5
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2024-06-13 19:40:18 +0100

    start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
+
 # Start Celery worker
 celery -A django_core worker --loglevel=info & 
 
 # Start Celery beat scheduler
-celery -A django_core beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler
+celery -A django_core beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
 # Start django server
-python -m gunicorn django_core.asgi:application -b 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker
+python -m gunicorn django_core.asgi:application -k uvicorn.workers.UvicornWorker
