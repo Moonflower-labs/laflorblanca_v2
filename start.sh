6034ea27aaa414bc8d6f7861ed6bcc388053ff8d
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2024-06-13 22:21:11 +0100

    detached start commands

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 
 
 # Start Celery worker
-celery -A django_core worker --loglevel=info & 
+celery -A django_core worker --loglevel=info --detach --pidfile=celeryworker.pid & 
 
 # Start Celery beat scheduler
-celery -A django_core beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
+celery -A django_core beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler --detach --pidfile=celerybeat.pid &
 
 # Start django server
 python -m gunicorn django_core.asgi:application -k uvicorn.workers.UvicornWorker
