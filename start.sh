aed3f14c717c7919cfd00257691ee77e66f6e98e
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2024-06-13 19:31:53 +0100

    added port to start.sh

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ celery -A django_core worker --loglevel=info &
 celery -A django_core beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler
 
 # Start django server
-python -m gunicorn django_core.asgi:application -k uvicorn.workers.UvicornWorker
+python -m gunicorn django_core.asgi:application -b 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker
