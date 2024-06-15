243dfc05e30aa1b85c6c1a2f6b3dd7ec9f45c013
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2024-06-15 13:10:58 +0100

    removed uvifrom start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
- 
 
-# # Start Celery worker
-# celery -A django_core worker --loglevel=info &
+# Start Celery worker
+celery -A django_core worker --loglevel=info &
 
-# # Start Celery beat scheduler
-# celery -A django_core beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
+# Start Celery beat scheduler
+celery -A django_core beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
 # Start Daphne for both HTTP and WebSocket traffic
-daphne -b 0.0.0.0 -p 1000 django_core.asgi:application &
-
-# Start Uvicorn to serve the ASGI application
-exec uvicorn django_core.asgi:application --host 0.0.0.0 --port 8001
-
-# Start uvicorn as the interface server to serve the ASGI application
-# uvicorn --host 0.0.0.0 --port 8001 --workers 4 --proxy-headers django_core.asgi:application
-
-# Start django server
-# python -m gunicorn django_core.asgi:application -k uvicorn.workers.UvicornWorker
+exec daphne -b 0.0.0.0 -p 1000 django_core.asgi:application
