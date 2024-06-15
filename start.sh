c78f3dfbeba70b45cc32124893fed9ff7deb75ff
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2024-06-15 11:04:47 +0100

    changes start script

--- start.sh
+++ start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# Start Daphne for both HTTP and WebSocket traffic
+daphne -b 0.0.0.0 -p 8000 django_core.asgi:application &
+
+# Delay to allow Daphne to start (adjust as needed)
+sleep 5
 
 # Start Celery worker
 celery -A django_core worker --loglevel=info & 
 
 # Start Celery beat scheduler
-celery -A django_core beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
-
-# Start Daphne for both HTTP and WebSocket traffic
-daphne django_core.asgi:application 
+celery -A django_core beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler
 
 # Start uvicorn as the interface server to serve the ASGI application
 # uvicorn --host 0.0.0.0 --port 8001 --workers 4 --proxy-headers django_core.asgi:application
