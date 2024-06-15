126c8b9c2c0681e95408154970cb18cc2b2f6e1d
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2024-06-15 10:54:56 +0100

    start chenges

--- start.sh
+++ start.sh
@@ -8,7 +8,10 @@ celery -A django_core worker --loglevel=info &
 celery -A django_core beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
 # Start Daphne for both HTTP and WebSocket traffic
-daphne -b 0.0.0.0 -p 8000 django_core.asgi:application &
+daphne django_core.asgi:application 
 
 # Start uvicorn as the interface server to serve the ASGI application
 # uvicorn --host 0.0.0.0 --port 8001 --workers 4 --proxy-headers django_core.asgi:application
+
+# Start django server
+# python -m gunicorn django_core.asgi:application -k uvicorn.workers.UvicornWorker
