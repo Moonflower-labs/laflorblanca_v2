5a0577897e18aecdf58851ce2c8b1e600ce5c2ee
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2024-06-20 00:36:06 +0100

    remove & for daphne

--- start.sh
+++ start.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-set -e
+set -e errexit
 
 # Start Daphne for both HTTP and WebSocket traffic
 daphne -b 0.0.0.0 -p 10000 django_core.asgi:application
 DAPHNE_PID=$!
 
 # Start Celery worker
-celery -A django_core worker --loglevel=info
+celery -A django_core worker --loglevel=info &
 CELERY_WORKER_PID=$!
 
 # Start Celery beat scheduler
-celery -A django_core beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler
+celery -A django_core beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 CELERY_BEAT_PID=$!
 
 # Wait for the processes to finish
