befa138d64429c3a4c7caade10cc33629a142959
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2024-06-20 00:22:30 +0100

    chsnge start.sh

--- start.sh
+++ start.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
-set -e errexit
+set -e
 
 # Start Daphne for both HTTP and WebSocket traffic
 daphne -b 0.0.0.0 -p 10000 django_core.asgi:application &
+DAPHNE_PID=$!
+
+# Start Celery worker
+celery -A django_core worker --loglevel=info &
+CELERY_WORKER_PID=$!
+
+# Start Celery beat scheduler
+celery -A django_core beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
+CELERY_BEAT_PID=$!
+
+# Wait for the processes to finish
+wait $DAPHNE_PID
+wait $CELERY_WORKER_PID
+wait $CELERY_BEAT_PID
+
+# # Start Celery worker
+# celery -A django_core worker --loglevel=info &
+
+
+
+
 # # Start Daphne for both HTTP and WebSocket traffic
 # daphne -b 127.0.0.1 -p $PORT django_core.asgi:application 
 
 # supervisord -c /etc/supervisor/conf.d/supervisord.conf
 # supervisord -c supervisord.conf
-
-
-# Start Celery worker
-celery -A django_core worker --loglevel=info &
-
-# Start Celery beat scheduler
-celery -A django_core beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler
-
-
-# # Start Celery worker
-# celery -A django_core worker --loglevel=info &
-
 
 
 
