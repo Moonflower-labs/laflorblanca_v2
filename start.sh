e0221a928432f9bcea72e89034756f8ccfff057e
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2024-06-20 20:55:35 +0100

    reverted supervisor attempt 500

--- start.sh
+++ start.sh
@@ -2,18 +2,18 @@
 
 set -e errexit
 
-# # Start Daphne for both HTTP and WebSocket traffic
-# daphne -b 0.0.0.0 -p 10000 django_core.asgi:application
+# Start Daphne for both HTTP and WebSocket traffic
+daphne -b 0.0.0.0 -p 10000 django_core.asgi:application
 
-# # Start Celery worker
-# celery -A django_core worker --loglevel=info -c 4 &
+# Start Celery worker
+celery -A django_core worker --loglevel=info -c 4 2>&1 &
 
-# # Start Celery beat scheduler
-# celery -A django_core beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
+# Start Celery beat scheduler
+celery -A django_core beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler 2>&1 &
 
 
 # supervisord -c /etc/supervisor/conf.d/supervisord.conf
-supervisord -c supervisord.conf
+# supervisord -c supervisord.conf
 
 
 # Start django server GUNICORN
