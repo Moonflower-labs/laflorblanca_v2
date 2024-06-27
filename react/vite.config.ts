41737179fde244519294a31152f8a60075f909ac
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2024-06-27 15:03:26 +0100

    any maskable

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
           "src": "maskable-icon-512x512.png",
           "sizes": "512x512",
           "type": "image/png",
-          "purpose": "maskable"
+          "purpose": "any maskable"
         }
       ]
       
